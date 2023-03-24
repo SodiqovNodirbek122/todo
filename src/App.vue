19cb71cfc86e77841a053d0d380a1e942519b06e
SodiqovNodirbek122 <nodirbeksodiqov122@gmail.com>
2023-03-24 10:33:42 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <Todo />
+  </div>
+</template>
+<script>
+import Todo from './components/Todo.vue'
+export default {
+  components: {
+    Todo,
+  },
+}
+</script>
---
+++ src/components/Todo.vue
@@ -0,0 +1,139 @@
+<template>
+  <div class="container" style="max-width: 600px">
+    <!-- Heading -->
+    <h2 class="text-center mt-5">My Vue Todo App</h2>
+
+    <!-- Input -->
+    <div class="d-flex mt-5">
+      <input
+        type="text"
+        v-model="task"
+        placeholder="Enter task"
+        class="w-100 form-control"
+      />
+      <button class="btn btn-warning" @click="submitTask">SUBMIT</button>
+    </div>
+
+    <!-- Task table -->
+    <table class="table table-bordered mt-5">
+      <thead>
+        <tr>
+          <th scope="col">Task</th>
+          <th scope="col" style="width: 120px">Status</th>
+          <th scope="col" class="text-center">#</th>
+          <th scope="col" class="text-center">#</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(task, index) in tasks" :key="index">
+          <td>
+            <span :class="{ 'line-through': task.status === 'finished' }">
+              {{ task.name }}
+            </span>
+          </td>
+          <td>
+            <span
+              class="pointer noselect"
+              @click="changeStatus(index)"
+              :class="{
+                'text-danger': task.status === 'to-do',
+                'text-success': task.status === 'finished',
+                'text-warning': task.status === 'in-progress',
+              }"
+            >
+              {{ task.status }}
+            </span>
+          </td>
+          <td class="text-center">
+            <div @click="deleteTask(index)">
+              <span class="fa fa-trash pointer"></span>
+            </div>
+          </td>
+          <td class="text-center">
+            <div @click="editTask(index)">
+              <p class="fa fa-pen pointer"></p>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted, computed, watch } from 'vue'
+
+const task = ref('')
+const editedTask = ref(null)
+const statuses = ref(['to-do', 'in-progress', 'finished'])
+const tasks = ref([])
+
+watch(task, (newVal) => {
+  localStorage.setItem('name', newVal)
+})
+
+watch(
+  tasks,
+  (newVal) => {
+    localStorage.setItem('todos', JSON.stringify(newVal))
+  },
+  {
+    deep: true,
+  }
+)
+
+onMounted(() => {
+  task.value = localStorage.getItem('name') || ''
+  tasks.value = JSON.parse(localStorage.getItem('todos')) || []
+})
+
+const changeStatus = (index) => {
+  let newIndex = statuses.value.indexOf(tasks.value[index].status)
+  if (++newIndex > 2) newIndex = 0
+  tasks.value[index].status = statuses.value[newIndex]
+}
+
+const deleteTask = (index) => {
+  tasks.value.splice(index, 1)
+}
+
+const editTask = (index) => {
+  task.value = tasks.value[index].name
+  editedTask.value = index
+}
+const submitTask = () => {
+  if (task.value.length === 0) return
+
+  /* We need to update the task */
+  if (editedTask.value != null) {
+    tasks.value[editedTask.value].name = task.value
+    editedTask.value = null
+  } else {
+    /* We need to add new task */
+    tasks.value.push({
+      name: task.value,
+      status: 'todo',
+    })
+  }
+
+  task.value = ''
+}
+</script>
+
+<style scoped>
+.pointer {
+  cursor: pointer;
+}
+.noselect {
+  -webkit-touch-callout: none; /* iOS Safari */
+  -webkit-user-select: none; /* Safari */
+  -khtml-user-select: none; /* Konqueror HTML */
+  -moz-user-select: none; /* Old versions of Firefox */
+  -ms-user-select: none; /* Internet Explorer/Edge */
+  user-select: none; /* Non-prefixed version, currently
+                                  supported by Chrome, Edge, Opera and Firefox */
+}
+.line-through {
+  text-decoration: line-through;
+}
+</style>
